f053074bedac038282fcc71a5f3d3191db095e47
Phuong Tran <phuong.tran@saigontechnology.vn>
2016-02-24 17:50:18 +0700

    Change get link 1

--- GetLink/Web/Controllers/GetLink1Controller.cs
+++ GetLink/Web/Controllers/GetLink1Controller.cs
@@ -13,37 +13,47 @@ namespace Web.Controllers
     public class GetLink1Controller : Controller
     {
         public ActionResult Index()
-        {
-            GetLink();
+        {//
+           // GetLink();
+
+            var web = new WebClient();
+            web.DownloadString(Congifs.DownloadLink);
+            //web.UploadValues(Congifs.DownloadLink);
+
             return View();
         }
 
         private void GetLink()
         {
             var cookies = new CookieCollection();
+            GetLinkHelper.Get(Congifs.LogoutUrl, ref cookies);
 
             // Login
-            var loginHtml = GetLinkHelper.Get(Congifs.LoginUrl, ref cookies);
-            var loginFsCsrf = GetLinkHelper.GetInputValue(GetLinkHelper.HtmlDocument(loginHtml), Congifs.FsCsrfName);
+            var loginHtml = GetLinkHelper.Get(Congifs.DownloadLink, ref cookies);
+            var loginFsCsrf = GetLinkHelper.FsLoginCsrfName(GetLinkHelper.HtmlDocument(loginHtml), Congifs.FsCsrfName);
             var postLoginData = string.Format(Congifs.RequestLoginParams, loginFsCsrf);
-            GetLinkHelper.Post(Congifs.LoginUrl, postLoginData, ref cookies);
+            var downloadHtml = GetLinkHelper.Post(Congifs.LoginUrl, postLoginData, ref cookies);
 
-            // Get link
-            
+            // Post download link
+            var fileFsCsrf = GetLinkHelper.FsDownloadCsrfName(GetLinkHelper.HtmlDocument(downloadHtml), Congifs.FsCsrfName);
+            var postDownloadData = string.Format(Congifs.RequestDownloadParams, fileFsCsrf);
+            GetLinkHelper.Post(Congifs.GetFileLink, postDownloadData, ref cookies);
+
             // Logout
+            GetLinkHelper.Get(Congifs.LogoutUrl, ref cookies);
         }
     }
 
     public static class Congifs
     {
-        public static string Email = HttpUtility.UrlEncode("tran.phuongvd02@gmail.com");
-        public static string Password = HttpUtility.UrlEncode("trUng zjn 12#");
         public static string LoginUrl = "https://www.fshare.vn/login";
+        public static string LogoutUrl = "https://www.fshare.vn/logout";
         public static string GetFileLink = "https://www.fshare.vn/download/get";
         public static string DownloadLink = "https://www.fshare.vn/file/T0R2T7630T/";
         public static string FsCsrfName = "fs_csrf";
         public static string LinkCodeName = "DownloadForm[linkcode]";
         public static string RequestLoginParams = "fs_csrf={0}&LoginForm%5Bemail%5D=tran.phuongvd02%40gmail.com&LoginForm%5Bpassword%5D=trUng+zjn+12%23&LoginForm%5Bcheckloginpopup%5D=0&LoginForm%5BrememberMe%5D=0&yt0=%C4%90%C4%83ng+nh%E1%BA%ADp";
+        public static string RequestDownloadParams = "fs_csrf={0}&DownloadForm%5Bpwd%5D=&DownloadForm%5Blinkcode%5D=T0R2T7630T&ajax=download-form&undefined=undefined";
     }
 
     public static class GetLinkHelper
@@ -55,9 +65,16 @@ namespace Web.Controllers
             return htmlDocument;
         }
 
-        public static string GetInputValue(HtmlDocument html, string inputName)
+        public static string FsLoginCsrfName(HtmlDocument html, string inputName)
         {
             var input = html.DocumentNode.Descendants("input").FirstOrDefault(x => x.Attributes != null && x.Attributes.Any(y => y.Value == inputName));
+
+            return input == null ? "" : input.Attributes["value"].Value;
+        }
+
+        public static string FsDownloadCsrfName(HtmlDocument html, string inputName)
+        {
+            var input = html.DocumentNode.Descendants("input").LastOrDefault(x => x.Attributes != null && x.Attributes.Any(y => y.Value == inputName));
 
             return input == null ? "" : input.Attributes["value"].Value;
         }
@@ -66,18 +83,19 @@ namespace Web.Controllers
         {
             // Basic
             request.Method = WebRequestMethods.Http.Post;
+            //request.AllowAutoRedirect = true;
             
             // Cookie
             request.CookieContainer = new CookieContainer();
             request.CookieContainer.Add(cookies);
 
             // IP address
-            request.ServicePoint.BindIPEndPointDelegate = delegate { return new IPEndPoint(IPAddress.Parse("192.168.0.102"), 0); };
+            //request.ServicePoint.BindIPEndPointDelegate = delegate { return new IPEndPoint(IPAddress.Parse("192.168.5.20"), 0); };
 
             // Headers
-            request.Accept = "ttext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
             request.ContentType = "application/x-www-form-urlencoded";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0";
 
             // Content length
             var byteArray = Encoding.ASCII.GetBytes(postData);
@@ -95,7 +113,10 @@ namespace Web.Controllers
             request.CookieContainer.Add(cookies);
             var response = (HttpWebResponse)request.GetResponse();
             var responseStream = response.GetResponseStream();
-            cookies = response.Cookies;
+            if (response.Cookies != null)
+            {
+                cookies.Add(response.Cookies);
+            }
             if (responseStream == null)
             {
                 return "";
@@ -107,7 +128,27 @@ namespace Web.Controllers
             }
         }
 
-        public static void Post(string url, string postData, ref CookieCollection cookies)
+        public static string Get(string url, CookieCollection cookies)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = new CookieContainer();
+            request.CookieContainer.Add(cookies);
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            if (responseStream == null)
+            {
+                return "";
+            }
+
+            using (var stream = new StreamReader(responseStream))
+            {
+                Debug.WriteLine("streamGet " + Environment.NewLine + stream.ReadToEnd());
+                return stream.ReadToEnd();
+            }
+        }
+
+        public static string Post(string url, string postData, ref CookieCollection cookies)
         {
             try
             {
@@ -117,21 +158,27 @@ namespace Web.Controllers
 
                 var response = (HttpWebResponse)request.GetResponse();
                 var responseStream = response.GetResponseStream();
-                cookies = response.Cookies;
-                //if (responseStream == null)
-                //{
-                //    return;
-                //}
+                if (response.Cookies != null)
+                {
+                    cookies.Add(response.Cookies);
+                }
+                if (responseStream == null)
+                {
+                    return "";
+                }
 
-                //using (var stream = new StreamReader(responseStream))
-                //{
-                //    Debug.WriteLine("stream " + Environment.NewLine + stream.ReadToEnd());
-                //}
+                using (var stream = new StreamReader(responseStream))
+                {
+                    //Debug.WriteLine("stream " + Environment.NewLine + stream.ReadToEnd());
+                    return stream.ReadToEnd();
+                }
             }
             catch (Exception ex)
             {
                 Debug.WriteLine("Post ERROR " + ex.Message);
             }
+
+            return "";
         } 
     }
 }
