c9e5792c79688ec080be6625ddb782b599ce6aa9
Phuong Tran <phuong.tran@saigontechnology.vn>
2016-03-30 17:41:55 +0700

    re-add

--- GetLink/Web/Controllers/GetLink1Controller.cs
+++ GetLink/Web/Controllers/GetLink1Controller.cs
@@ -25,7 +25,7 @@ namespace Web.Controllers
 
         private void GetLink()
         {
-            var cookies = new CookieCollection();
+            var cookies = new CookieContainer();
             GetLinkHelper.Get(Configs.LogoutUrl, ref cookies);
 
             // Login
@@ -44,129 +44,5 @@ namespace Web.Controllers
         }
     }
 
-    public static class GetLinkHelper
-    {
-        public static HtmlDocument HtmlDocument(string html)
-        {
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
-            return htmlDocument;
-        }
-
-        public static string FsLoginCsrfName(HtmlDocument html, string inputName)
-        {
-            var input = html.DocumentNode.Descendants("input").FirstOrDefault(x => x.Attributes != null && x.Attributes.Any(y => y.Value == inputName));
-
-            return input == null ? "" : input.Attributes["value"].Value;
-        }
-
-        public static string FsDownloadCsrfName(HtmlDocument html, string inputName)
-        {
-            var input = html.DocumentNode.Descendants("input").LastOrDefault(x => x.Attributes != null && x.Attributes.Any(y => y.Value == inputName));
-
-            return input == null ? "" : input.Attributes["value"].Value;
-        }
-
-        public static void InitPostRequest(HttpWebRequest request, CookieCollection cookies, string postData)
-        {
-            // Basic
-            request.Method = WebRequestMethods.Http.Post;
-            //request.AllowAutoRedirect = true;
-            
-            // Cookie
-            request.CookieContainer = new CookieContainer();
-            request.CookieContainer.Add(cookies);
-
-            // IP address
-            //request.ServicePoint.BindIPEndPointDelegate = delegate { return new IPEndPoint(IPAddress.Parse("192.168.5.20"), 0); };
-
-            // Headers
-            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0";
-
-            // Content length
-            var byteArray = Encoding.ASCII.GetBytes(postData);
-            request.ContentLength = byteArray.Length;
-
-            var newStream = request.GetRequestStream();
-            newStream.Write(byteArray, 0, byteArray.Length);
-            newStream.Close();
-        }
-
-        public static string Get(string url, ref CookieCollection cookies)
-        {
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.CookieContainer = new CookieContainer();
-            request.CookieContainer.Add(cookies);
-            var response = (HttpWebResponse)request.GetResponse();
-            var responseStream = response.GetResponseStream();
-            if (response.Cookies != null)
-            {
-                cookies.Add(response.Cookies);
-            }
-            if (responseStream == null)
-            {
-                return "";
-            }
-
-            using (var stream = new StreamReader(responseStream))
-            {
-                return stream.ReadToEnd();
-            }
-        }
-
-        public static string Get(string url, CookieCollection cookies)
-        {
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.CookieContainer = new CookieContainer();
-            request.CookieContainer.Add(cookies);
-
-            var response = (HttpWebResponse)request.GetResponse();
-            var responseStream = response.GetResponseStream();
-            if (responseStream == null)
-            {
-                return "";
-            }
-
-            using (var stream = new StreamReader(responseStream))
-            {
-                Debug.WriteLine("streamGet " + Environment.NewLine + stream.ReadToEnd());
-                return stream.ReadToEnd();
-            }
-        }
-
-        public static string Post(string url, string postData, ref CookieCollection cookies)
-        {
-            try
-            {
-                var request = (HttpWebRequest)WebRequest.Create(url);
-
-                InitPostRequest(request, cookies, postData);
-
-                var response = (HttpWebResponse)request.GetResponse();
-                var responseStream = response.GetResponseStream();
-                if (response.Cookies != null)
-                {
-                    cookies.Add(response.Cookies);
-                }
-                if (responseStream == null)
-                {
-                    return "";
-                }
-
-                using (var stream = new StreamReader(responseStream))
-                {
-                    //Debug.WriteLine("stream " + Environment.NewLine + stream.ReadToEnd());
-                    return stream.ReadToEnd();
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Post ERROR " + ex.Message);
-            }
-
-            return "";
-        } 
-    }
+    
 }
---
+++ GetLink/Web/Controllers/Getlink3Controller.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using Web.Helpers;
+
+namespace Web.Controllers
+{
+    public class Getlink3Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            HttpWebRequest request;
+            HttpWebResponse response;
+            CookieContainer cookies;
+            string html;
+            string csrf;
+            string postData;
+
+            // csrf
+            request = (HttpWebRequest)WebRequest.Create(Configs.DownloadLink);
+            request.AllowAutoRedirect = false;
+            request.CookieContainer = new CookieContainer();
+            response = (HttpWebResponse)request.GetResponse();
+            cookies = request.CookieContainer;
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                html = stream.ReadToEnd();
+            }
+            csrf = GetLinkHelper.FsLoginCsrfName(GetLinkHelper.HtmlDocument(html), Configs.FsCsrfName);
+            response.Close();
+
+            // Login
+            postData = string.Format(Configs.RequestLoginParams, csrf);
+            request = (HttpWebRequest)WebRequest.Create(Configs.LoginUrl);
+            request.AllowAutoRedirect = false;
+            request.Method = WebRequestMethods.Http.Post;
+            request.CookieContainer = cookies;
+            
+
+            // Headers
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0";
+            //request.Headers.Add("Accept-Encoding", "gzip");
+
+            // Content length
+            var byteArray = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = byteArray.Length;
+
+            var newStream = request.GetRequestStream();
+            newStream.Write(byteArray, 0, byteArray.Length);
+            newStream.Close();
+
+            response = (HttpWebResponse)request.GetResponse();
+            cookies = request.CookieContainer;
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                html = stream.ReadToEnd();
+            }
+            response.Close();
+
+            // Reload download file page
+            request = (HttpWebRequest)WebRequest.Create(Configs.DownloadLink);
+            request.AllowAutoRedirect = false;
+            request.CookieContainer = cookies;
+            response = (HttpWebResponse)request.GetResponse();
+            cookies = request.CookieContainer;
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                html = stream.ReadToEnd();
+            }
+            csrf = GetLinkHelper.FsLoginCsrfName(GetLinkHelper.HtmlDocument(html), Configs.FsCsrfName);
+            response.Close();
+
+            // Download file
+            postData = string.Format(Configs.RequestDownloadParams, csrf);
+            request = (HttpWebRequest)WebRequest.Create(Configs.GetFileLink);
+            request.AllowAutoRedirect = false;
+            request.Method = WebRequestMethods.Http.Post;
+            request.CookieContainer = cookies;
+
+            // Headers
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0";
+            //request.Headers.Add("Accept-Encoding", "gzip");
+
+            // Content length
+            byteArray = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = byteArray.Length;
+
+            newStream = request.GetRequestStream();
+
+            newStream.Write(byteArray, 0, byteArray.Length);
+            newStream.Close();
+
+            response = (HttpWebResponse)request.GetResponse();
+            cookies = request.CookieContainer;
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                html = stream.ReadToEnd();
+            }
+            response.Close();
+
+
+            return View();
+        }
+    }
+}
